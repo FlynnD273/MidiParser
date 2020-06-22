2104176dde443d804257c0fade56b2573af6ecdb
Flynn Duniho <flynn.duniho@bellevuecollege.edu>
2020-06-22 14:12:59 -0700

    Changed Note class

--- MidiParser/Note.cs
+++ MidiParser/Note.cs
@@ -6,10 +6,14 @@ namespace MidiParser
 {
     class Note
     {
-        public int NotePitch { get; private set; }
-        public long TimeStart { get; private set; }
-        public int Length { get; private set; }
-        public int Velocity { get; private set; }
+        private int _notePitch;
+        public int NotePitch { get => _notePitch; private set => _notePitch = value; }
+        private long _timeStart;
+        public long TimeStart { get => _timeStart; private set => _timeStart = value; }
+        private int _length;
+        public int Length { get => _length; private set => _length = value; }
+        private int _velocity;
+        public int Velocity { get => _velocity; private set => _velocity = value; }
 
         public Note (int note, long timeStart, int length, int velocity)
         {
