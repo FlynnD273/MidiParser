0cf8deccf01ec20e2884d19d78bcd54a497c53c6
Flynn Duniho <flynn.duniho@bellevuecollege.edu>
2020-07-01 14:11:02 -0700

    Now calculates time in seconds. If more than one files is processed, generate a list file that Scratch recognizes.

--- MidiParser/Note.cs
+++ MidiParser/Note.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NAudio.Midi;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,19 +9,24 @@ namespace MidiParser
     {
         private int _notePitch;
         public int NotePitch { get => _notePitch; private set => _notePitch = value; }
-        private long _timeStart;
-        public long TimeStart { get => _timeStart; private set => _timeStart = value; }
-        private int _length;
-        public int Length { get => _length; private set => _length = value; }
+        private double _timeStart;
+        public double TimeStart { get => _timeStart; private set => _timeStart = value; }
+        private double _length;
+        public double Length { get => _length; private set => _length = value; }
         private int _velocity;
         public int Velocity { get => _velocity; private set => _velocity = value; }
 
-        public Note (int note, long timeStart, int length, int velocity)
+        public Note (int note, double timeStart, double length, int velocity)
         {
             NotePitch = note;
             TimeStart = timeStart;
             Length = length;
             Velocity = velocity;
+        }
+
+        public static double ToSeconds (long time, TempoEvent lastTempoEvent, int ticksPerQuarterNote)
+        {
+            return (double)(((double)(time - lastTempoEvent.AbsoluteTime) / ticksPerQuarterNote) * lastTempoEvent.MicrosecondsPerQuarterNote + lastTempoEvent.AbsoluteTime) / 1000000;
         }
     }
 }
--- MidiParser/Program.cs
+++ MidiParser/Program.cs
@@ -13,70 +13,154 @@ namespace MidiParser
         static void Main(string[] args)
         {
             //Iterate through all files dropped onto the program
-            for (int i = 0; i < args.Length; i++)
+            switch (args.Length)
             {
-                //Make sure the file exists
-                if (File.Exists(args[i]))
-                {
-                    string path = args[i];
-                    string output = Path.Combine(Path.GetDirectoryName(path), "_" + Path.GetFileNameWithoutExtension(path) + " - Converted.txt");
-                    MidiFile mid;
-
-                    //If an error is thrown, notify user and continue looping
-                    try
+                case 1:
+                    ConvertFile(args[0]);
+                    break;
+                case 0:
+                    while (true)
                     {
-                        mid = new MidiFile(path);
+                        Console.WriteLine("Paste file path to convert here (Enter a blank line to quit):");
+                        string s = Console.ReadLine();
+                        if (!s.Equals(""))
+                            ConvertFile(s);
+                        else
+                            break;
+                    }
+                    break;
+                default:
+                    ConvertAllFiles(args);
+                    break;
+            }
+        }
 
-                        //Combine all MIDI layers
-                        mid.Events.MidiFileType = 0;
+        private static void ConvertAllFiles (string[] files)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string file in files)
+            {
+                sb.AppendLine(ConvertFile(file));
+            }
+            sb.Remove(sb.Length - 1, 1);
 
-                        List<Note> notes = new List<Note>();
+            File.WriteAllText(Path.Combine(Path.GetDirectoryName(files[0]), "__AllSongs.txt"), sb.ToString());
+        }
 
-                        //Iterate through every note press in the file
-                        foreach (MidiEvent midiEvent in mid.Events[0])
+        private static string ConvertFile (string file)
+        {
+            //Make sure the file exists
+            if (File.Exists(file))
+            {
+                string path = file;
+                string output = Path.Combine(Path.GetDirectoryName(path), "_" + Path.GetFileNameWithoutExtension(path) + " - Converted.txt");
+                MidiFile mid;
+
+                //If an error is thrown, notify user and continue looping
+                try
+                {
+                    mid = new MidiFile(path);
+                    int ticksPerQuarterNote = mid.DeltaTicksPerQuarterNote;
+
+                    List<TempoEvent> tempoEvents = new List<TempoEvent>();
+                    tempoEvents.Add(new TempoEvent(500000, 0));
+
+                    MidiEvent[][] midiEvents = new MidiEvent[mid.Events.Count()][];
+
+                    for (int i = 0; i < midiEvents.Length; i++)
+                    {
+                        midiEvents[i] = mid.Events.ElementAt(i).ToArray();
+                    }
+
+                    List<Note> notes = new List<Note>();
+
+                    foreach (MidiEvent m in midiEvents[0])
+                    {
+                        if (m is TempoEvent)
                         {
+                            if (tempoEvents.Last().MicrosecondsPerQuarterNote != ((TempoEvent)m).MicrosecondsPerQuarterNote)
+                                tempoEvents.Add(m as TempoEvent);
+                        }
+                    }
+
+                    //Iterate through every note press in the file
+                    for (int i = 0; i < midiEvents.Length; i++)
+                    {
+                        //Skip drums
+                        if (i == 9)
+                        {
+                            continue;
+                        }
+
+                        int currentTempoIndex = 0;
+
+                        foreach (MidiEvent midiEvent in midiEvents[i])
+                        {
+                            if (currentTempoIndex < tempoEvents.Count - 1)
+                            {
+                                while (tempoEvents[currentTempoIndex + 1].AbsoluteTime < midiEvent.AbsoluteTime)
+                                {
+                                    if (currentTempoIndex < tempoEvents.Count - 1)
+                                        break;
+
+                                    currentTempoIndex++;
+                                }
+                            }
+
                             //Only if a note press
                             if (midiEvent.CommandCode == MidiCommandCode.NoteOn)
                             {
-                                NoteOnEvent note = (NoteOnEvent)midiEvent;
+                                NoteOnEvent note = midiEvent as NoteOnEvent;
 
                                 //If not an off note
                                 if (note.Velocity != 0)
                                 {
+                                    double timeInSeconds = Note.ToSeconds(note.AbsoluteTime, tempoEvents[currentTempoIndex], ticksPerQuarterNote);
+                                    double lengthInSeconds = Note.ToSeconds(note.NoteLength, tempoEvents[currentTempoIndex], ticksPerQuarterNote);
                                     //Add this note
-                                    notes.Add(new Note(note.NoteNumber, note.AbsoluteTime, note.NoteLength, note.Velocity));
+                                    notes.Add(new Note(note.NoteNumber, timeInSeconds, lengthInSeconds, note.Velocity));
                                 }
                             }
                         }
-
-                        //Sort by time in ascending order
-                        Note[] sortedNotes = notes.OrderBy(o => o.TimeStart).ToArray();
-
-                        //String to write to the text file
-                        StringBuilder info = new StringBuilder($"{Path.GetFileNameWithoutExtension(path)},1,");
-
-                        foreach (Note n in sortedNotes)
-                        {
-                            //Every 4 items contains all the info for a note
-                            info.Append(((double)n.TimeStart / 32).ToString()).Append(",");
-                            info.Append(n.NotePitch.ToString()).Append(",");
-                            info.Append(n.Length.ToString()).Append(",");
-                            info.Append(n.Velocity.ToString()).Append(",");
-                        }
-
-                        //Remove end comma
-                        info.Remove(info.Length - 1, 1);
-
-                        //Write to a text file
-                        File.WriteAllText(output, info.ToString());
                     }
-                    catch (Exception e)
+
+                    //Sort by time in ascending order
+                    Note[] sortedNotes = notes.OrderBy(o => o.TimeStart).ToArray();
+
+                    char separateChar = '\\';
+                    //String to write to the text file
+                    //Start with the file name without commas as the song title
+                    StringBuilder info = new StringBuilder($"{Path.GetFileNameWithoutExtension(path).Replace(separateChar.ToString(), "")}{separateChar}1{separateChar}");
+
+                    foreach (Note n in sortedNotes)
                     {
-                        Console.WriteLine($"Could not process file {args[i]}\n{e.Message}\nPress enter to continue");
-                        Console.ReadLine();
+                        //Every 4 items contains all the info for a note
+                        info.Append(n.TimeStart.ToString()).Append(separateChar);
+                        info.Append(n.NotePitch.ToString()).Append(separateChar);
+                        info.Append(n.Length.ToString()).Append(separateChar);
+                        info.Append(n.Velocity.ToString()).Append(separateChar);
                     }
+
+                    //Remove end comma
+                    info.Remove(info.Length - 1, 1);
+
+                    //Write to a text file
+                    File.WriteAllText(output, info.ToString());
+
+                    return info.ToString();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Could not process file {file}\n{e.Message}\nPress enter to continue");
+                    Console.ReadLine();
                 }
             }
+            else
+            {
+                Console.WriteLine($"File {file} does not exist.\nPress enter to continue");
+                Console.ReadLine();
+            }
+            return null;
         }
     }
 }
